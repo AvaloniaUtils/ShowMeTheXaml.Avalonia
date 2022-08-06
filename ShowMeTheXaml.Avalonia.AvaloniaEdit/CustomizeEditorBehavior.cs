0af565ffdefa689aaee32acb67b626cac4123f18
SKProCH <skproch@yandex.ru>
2022-08-06 22:41:56 +0300

    Rework and improve AvaloniaEdit

---
+++ ShowMeTheXaml.Avalonia.AvaloniaEdit/CustomizeEditorBehavior.cs
@@ -0,0 +1,35 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Xaml.Interactivity;
+using AvaloniaEdit;
+
+namespace ShowMeTheXaml.Avalonia.AvaloniaEdit; 
+
+/// <remarks>
+/// SUITABLE ONLY FOR THIS PROJECT
+/// </remarks>
+public class CustomizeEditorBehavior : Behavior<TextEditor> {
+    #region No width reduction
+
+    private double originalMinWidth = 0;
+    private IDisposable? _boundsChangedObservable;
+    protected override void OnAttachedToVisualTree() {
+        base.OnAttachedToVisualTree();
+        originalMinWidth = AssociatedObject.MinWidth;
+        _boundsChangedObservable = AssociatedObject.GetObservable(Visual.BoundsProperty)
+            .Subscribe(OnBoundsChanged);
+    }
+    private void OnBoundsChanged(Rect obj) {
+        AssociatedObject.MinWidth = Math.Clamp(obj.Width, AssociatedObject.MinWidth, AssociatedObject.MaxWidth);
+    }
+
+    protected override void OnDetachedFromVisualTree() {
+        _boundsChangedObservable?.Dispose();
+        AssociatedObject.MinWidth = originalMinWidth;
+        base.OnDetachedFromVisualTree();
+    }
+
+    #endregion
+}
