3412108837be4813130aa3f9ee28d28b180a3d7d
SKProCH <skproch@yandex.ru>
2022-09-14 23:55:34 +0300

    Improve AvaloniaRuntimeXamlLoaderHelper

--- ShowMeTheXaml.Avalonia/AvaloniaRuntimeXamlLoaderHelper.cs
+++ ShowMeTheXaml.Avalonia/AvaloniaRuntimeXamlLoaderHelper.cs
@@ -5,7 +5,7 @@ using System.Xml;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace ShowMeTheXaml; 
+namespace ShowMeTheXaml;
 
 /// <summary>
 /// Experimental AvaloniaRuntimeXamlLoader helper, allows to compile xaml with separated namespace declarations
@@ -16,7 +16,7 @@ public static class AvaloniaRuntimeXamlLoaderHelper {
     {1}
     </ContentControl>
     """;
-    
+
     public static object Parse(string content, Dictionary<string, string> namespaces) {
         string FormatNamespace(KeyValuePair<string, string> pair)
             => string.IsNullOrEmpty(pair.Key) ? $"xmlns=\"{pair.Value}\"" : $"xmlns:{pair.Key}=\"{pair.Value}\"";
@@ -24,8 +24,27 @@ public static class AvaloniaRuntimeXamlLoaderHelper {
         var namespacesString = string.Join(" ", namespaces.Select(FormatNamespace));
         return Parse(content, namespacesString);
     }
-    
+
     public static object Parse(string content, string namespaces) {
+        try {
+            // Try to insert namespaces before closing first tag
+            var endTagIndex = content.IndexOf('>');
+            // No closing symbol (>), this is just a string probably
+            if (endTagIndex == -1) return content;
+
+            var slashAndMoreThenIndex = content.IndexOf("/>", StringComparison.InvariantCulture);
+            // This is a probably one tag without content
+            if (slashAndMoreThenIndex != -1) endTagIndex = Math.Min(endTagIndex, slashAndMoreThenIndex);
+            var finalXaml = content.Insert(endTagIndex, " " + namespaces);
+            return AvaloniaRuntimeXamlLoader.Parse<object>(finalXaml);
+        }
+        catch (Exception) {
+            // Falling back to old method
+            return ContentControlWrapperParse(content, namespaces);
+        }
+    }
+
+    private static object ContentControlWrapperParse(string content, string namespaces) {
         var finalXaml = string.Format(XamlTemplate, namespaces, content);
         try {
             var contentControl = AvaloniaRuntimeXamlLoader.Parse<ContentControl>(finalXaml);
@@ -34,10 +53,10 @@ public static class AvaloniaRuntimeXamlLoaderHelper {
         catch (XmlException e) {
             // cut line info
             var lastIndexOfDot = e.Message.LastIndexOf('.', e.Message.Length - 2);
-            var meaningfulMessage = lastIndexOfDot == -1 
-                ? e.Message 
+            var meaningfulMessage = lastIndexOfDot == -1
+                ? e.Message
                 : e.Message.Substring(0, lastIndexOfDot + 1);
-            
+
             throw new XmlException(meaningfulMessage, e.InnerException, e.LineNumber - 1, e.LinePosition);
         }
     }
