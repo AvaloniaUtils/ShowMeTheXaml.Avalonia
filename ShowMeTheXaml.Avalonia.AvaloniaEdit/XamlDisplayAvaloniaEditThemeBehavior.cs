b48e605b8bcac34806d559d2eca3913854890b1a
SKProCH <skproch@yandex.ru>
2023-10-15 18:27:48 +0300

    Fixes CodeHighlightThemeNameProperty resolving

--- ShowMeTheXaml.Avalonia.AvaloniaEdit/XamlDisplayAvaloniaEditThemeBehavior.cs
+++ ShowMeTheXaml.Avalonia.AvaloniaEdit/XamlDisplayAvaloniaEditThemeBehavior.cs
@@ -1,5 +1,7 @@
 using System;
 using Avalonia;
+using Avalonia.LogicalTree;
+using Avalonia.VisualTree;
 using Avalonia.Xaml.Interactivity;
 using AvaloniaEdit;
 using AvaloniaEdit.TextMate;
@@ -9,25 +11,21 @@ namespace ShowMeTheXaml.Avalonia.AvaloniaEdit;
 
 public class XamlDisplayAvaloniaEditThemeBehavior : Behavior<TextEditor> {
     private IDisposable? _disposable;
-    public static readonly AttachedProperty<ThemeName> CodeHighlightThemeNameProperty =
-        XamlDisplayAvaloniaEdit.CodeHighlightThemeNameProperty.AddOwner<XamlDisplayAvaloniaEditThemeBehavior>();
     private RegistryOptions? _registryOptions;
     private TextMate.Installation? _textMateInstallation;
-
-    public ThemeName CodeHighlightThemeName {
-        get => GetValue(CodeHighlightThemeNameProperty);
-        set => SetValue(CodeHighlightThemeNameProperty, value);
-    }
 
     /// <inheritdoc />
     protected override void OnAttachedToVisualTree() {
         base.OnAttachedToVisualTree();
-        
-        _registryOptions = new RegistryOptions(CodeHighlightThemeName);
+
+        var xamlDisplay = AssociatedObject.FindLogicalAncestorOfType<XamlDisplay>()!;
+        var themeName = xamlDisplay.GetValue(XamlDisplayAvaloniaEdit.CodeHighlightThemeNameProperty);
+
+        _registryOptions = new RegistryOptions(themeName);
         _textMateInstallation = AssociatedObject!.InstallTextMate(_registryOptions);
         _textMateInstallation.SetGrammar(_registryOptions.GetScopeByLanguageId("xml"));
-        
-        _disposable = this.GetObservable(CodeHighlightThemeNameProperty)
+
+        _disposable = xamlDisplay.GetObservable(XamlDisplayAvaloniaEdit.CodeHighlightThemeNameProperty)
             .Subscribe(name => {
                 _textMateInstallation.SetTheme(_registryOptions.LoadTheme(name));
             });
