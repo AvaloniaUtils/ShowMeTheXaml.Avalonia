f99a80770b941efa2b6a18431b5e388db397b9b7
SKProCH <skproch@yandex.ru>
2024-04-26 17:43:15 +0300

    Downgrade source generator TFM to netstandard2.0 to bypass the VS bullshit

--- ShowMeTheXaml.Avalonia/XamlDisplay.xaml.cs
+++ ShowMeTheXaml.Avalonia/XamlDisplay.xaml.cs
@@ -11,8 +11,10 @@ using Avalonia.Media;
 using Avalonia.Metadata;
 
 // ReSharper disable once CheckNamespace
-namespace ShowMeTheXaml {
-    public class XamlDisplay : TemplatedControl {
+namespace ShowMeTheXaml
+{
+    public class XamlDisplay : TemplatedControl
+    {
         public static readonly StyledProperty<string?> XamlTextProperty =
             AvaloniaProperty.Register<XamlDisplay, string?>(nameof(XamlText));
 
@@ -22,7 +24,7 @@ namespace ShowMeTheXaml {
         public static readonly StyledProperty<AlignmentY> XamlButtonAlignmentProperty =
             AvaloniaProperty.Register<XamlDisplay, AlignmentY>(nameof(XamlButtonAlignment), AlignmentY.Bottom);
 
-        public static readonly StyledProperty<bool> IsEditableProperty = 
+        public static readonly StyledProperty<bool> IsEditableProperty =
             AvaloniaProperty.Register<XamlDisplay, bool>("IsEditable", true);
 
         private IDisposable? _buttonClickHandler;
@@ -31,38 +33,49 @@ namespace ShowMeTheXaml {
 
         public XamlDisplay()
         {
+#if NETSTANDARD2_1_OR_GREATER
             IsEditable = RuntimeFeature.IsDynamicCodeSupported;
+#else
+            IsEditable = false;
+#endif
         }
 
-        public bool IsEditable {
+        public bool IsEditable
+        {
             get => GetValue(IsEditableProperty);
             set => SetValue(IsEditableProperty, value);
         }
 
-        public string UniqueId {
+        public string UniqueId
+        {
             get => _uniqueId;
-            set {
+            set
+            {
                 _uniqueId = value;
                 Reset();
             }
         }
 
-        public string? XamlText {
+        public string? XamlText
+        {
             get => GetValue(XamlTextProperty);
             set => SetValue(XamlTextProperty, value);
         }
 
         [Content]
-        public object? Content {
+        public object? Content
+        {
             get => GetValue(ContentProperty);
-            set {
+            set
+            {
                 if (GetValue(ContentProperty) is ILogical oldLogical) LogicalChildren.Remove(oldLogical);
                 SetValue(ContentProperty, value);
                 if (value is ILogical newLogical) LogicalChildren.Add(newLogical);
             }
         }
 
-        public AlignmentY XamlButtonAlignment {
+        public AlignmentY XamlButtonAlignment
+        {
             get => GetValue(XamlButtonAlignmentProperty);
             set => SetValue(XamlButtonAlignmentProperty, value);
         }
@@ -70,24 +83,32 @@ namespace ShowMeTheXaml {
         public Dictionary<string, string> CurrentFileNamespaceAliases =>
             XamlFilesNamespaceAliases[DisplayContent[UniqueId].FileName];
 
-        private void SourceXamlButtonOnPointerPressed(object sender, PointerPressedEventArgs e) {
-            if (_popup != null) {
+        private void SourceXamlButtonOnPointerPressed(object sender, PointerPressedEventArgs e)
+        {
+            if (_popup != null)
+            {
                 _popup.IsOpen = !_popup.IsOpen;
             }
         }
 
-        protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
             base.OnApplyTemplate(e);
             _buttonClickHandler?.Dispose();
             _popup = e.NameScope.Find<Popup>("XamlPopup");
-            _buttonClickHandler = e.NameScope.Find<Control>("SourceXamlButton").AddDisposableHandler(PointerPressedEvent, SourceXamlButtonOnPointerPressed);
+            _buttonClickHandler = e.NameScope.Find<Control>("SourceXamlButton")
+                .AddDisposableHandler(PointerPressedEvent, SourceXamlButtonOnPointerPressed);
         }
 
-        public void Reset() {
+        public void Reset()
+        {
             if (!DisplayContent.TryGetValue(UniqueId, out var xamlDisplayInstanceData)) return;
-            if (!string.IsNullOrEmpty(XamlText)) {
-                Content = AvaloniaRuntimeXamlLoaderHelper.Parse(xamlDisplayInstanceData.Data, CurrentFileNamespaceAliases);
+            if (!string.IsNullOrEmpty(XamlText))
+            {
+                Content = AvaloniaRuntimeXamlLoaderHelper.Parse(xamlDisplayInstanceData.Data,
+                    CurrentFileNamespaceAliases);
             }
+
             XamlText = xamlDisplayInstanceData.Data;
         }
 
@@ -96,17 +117,21 @@ namespace ShowMeTheXaml {
         private static Dictionary<string, XamlDisplayInstanceData>? _displayContent;
         private static Dictionary<string, Dictionary<string, string>>? _xamlFilesNamespaceAliases;
 
-        public static Dictionary<string, XamlDisplayInstanceData> DisplayContent {
+        public static Dictionary<string, XamlDisplayInstanceData> DisplayContent
+        {
             get => _displayContent
-                ?? throw new NullReferenceException("Install ShowMeTheXaml.Avalonia.Generator and call XamlDisplayInternalData.RegisterXamlDisplayData" +
-                                                    "Also check \"Getting started\" on our Github");
+                   ?? throw new NullReferenceException(
+                       "Install ShowMeTheXaml.Avalonia.Generator and call XamlDisplayInternalData.RegisterXamlDisplayData" +
+                       "Also check \"Getting started\" on our Github");
             set => _displayContent = value;
         }
 
-        public static Dictionary<string, Dictionary<string, string>> XamlFilesNamespaceAliases {
+        public static Dictionary<string, Dictionary<string, string>> XamlFilesNamespaceAliases
+        {
             get => _xamlFilesNamespaceAliases
-                ?? throw new NullReferenceException("Install ShowMeTheXaml.Avalonia.Generator and call XamlDisplayInternalData.RegisterXamlDisplayData" +
-                                                    "Also check \"Getting started\" on our Github");
+                   ?? throw new NullReferenceException(
+                       "Install ShowMeTheXaml.Avalonia.Generator and call XamlDisplayInternalData.RegisterXamlDisplayData" +
+                       "Also check \"Getting started\" on our Github");
             set => _xamlFilesNamespaceAliases = value;
         }
 
